e79c4af5bf25ef393e5c0dd5b6150a59d98b4343
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-01-08 15:22:14 +0900

    ワンタイムパスワードを secret_str.txt とパスフレーズから生成するスクリプトを作成
    第一引数に名前、第二引数にパスフレーズを

---
+++ clip-onetimepass.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+set -ue -o pipefail
+
+SCRIPT_DIR="$(cd $(dirname $0); pwd)"
+SECRET_STR_TXT="${SCRIPT_DIR}/secret_str.txt"
+
+function main(){
+    local name="${1:-}"
+    local pass_phrase="${2:-}"
+    check_args "${name}" "${pass_phrase}"
+    local crypted_secret_str="$(cat "${SECRET_STR_TXT}" | grep "${name}" | grep -oE "[^ ]+$")"
+    local secret_str="$(echo "${crypted_secret_str}" | openssl enc -d -des -base64 -A -k "${pass_phrase}")"
+    oathtool --totp -d 6 --time-step-size=30s --base32 "${secret_str}"
+    return
+}
+
+function check_args(){
+    local name="${1:-}"
+    local pass_phrase="${2:-}"
+    if [ "${name}" == "" ]; then
+        echo "name (1st argument) is empty. abort process."
+        exit 1
+    fi
+    if echo "${name}" | grep -q " "; then
+        echo "name (1st argument) is invalid. Don't include space. abort process."
+        exit 1
+    fi
+    if [ "${pass_phrase}" == "" ]; then
+        echo "pass phrase (2nd argument) is empty. abort process."
+        exit 1
+    fi
+}
+
+
+
+main "${@}"
+exit 0
+
